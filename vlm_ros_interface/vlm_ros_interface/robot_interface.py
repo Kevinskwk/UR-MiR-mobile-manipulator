43cc804b1d8ec1cd0543c6b9fb287b21a9c9604c
kevinskwk <kevinskwk@gmail.com>
2024-12-01 17:13:46 +0800

    Add vacuum gripper

--- vlm_ros_interface/vlm_ros_interface/robot_interface.py
+++ vlm_ros_interface/vlm_ros_interface/robot_interface.py
@@ -4,15 +4,33 @@ from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped, Quaternion, Pose
 from nav2_msgs.action import NavigateToPose
 from tf_transformations import quaternion_from_euler
+from linkattacher_msgs.srv import AttachLink, DetachLink
 
 import threading
 
 
 class RobotInterface(Node):
-    def __init__(self, node_name="navigation_client"):
+    def __init__(self, node_name="robot_interface"):
         super().__init__(node_name)
         self._nav_client = ActionClient(self, NavigateToPose, 'navigate_to_pose')
         self.moveit_publisher = self.create_publisher(PoseStamped, 'moveit_poses', 10)
+        self.attach_client = self.create_client(AttachLink, '/ATTACHLINK')
+        self.detach_client = self.create_client(DetachLink, '/DETACHLINK')
+
+        while not self.attach_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting again...')
+        while not self.detach_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting again...')
+
+        self.attach_request = AttachLink.Request()
+        self.attach_request.model1_name = 'mir_robot'
+        self.attach_request.link1_name = 'ur_wrist_3_link'
+        self.attach_request.link2_name = 'link_0'
+
+        self.detach_request = DetachLink.Request()
+        self.detach_request.model1_name = 'mir_robot'
+        self.detach_request.link1_name = 'ur_wrist_3_link'
+        self.detach_request.link2_name = 'link_0'
 
     def send_navigation_goal(self, x, y, yaw):
         """Send a navigation goal to Nav2."""
@@ -46,6 +64,30 @@ class RobotInterface(Node):
 
         self.moveit_publisher.publish(pose_stamped)
 
+    def send_attach_call(self, obj_name):
+        self.attach_request.model2_name = obj_name
+        self.attach_client.call_async(self.attach_request)
+        # self.future.add_done_callback(self.attach_response_callback)
+
+    def send_detach_call(self, obj_name):
+        self.detach_request.model2_name = obj_name
+        self.detach_client.call_async(self.detach_request)
+        # self.future.add_done_callback(self.detach_response_callback)
+
+    def attach_response_callback(self, future):
+        try:
+            response = future.result()
+            self.get_logger().info(f"Service call successful, attached link: {response.success}")
+        except Exception as e:
+            self.get_logger().error(f"Service call failed: {e}")
+
+    def detach_response_callback(self, future):
+        try:
+            response = future.result()
+            self.get_logger().info(f"Service call successful, detached link: {response.success}")
+        except Exception as e:
+            self.get_logger().error(f"Service call failed: {e}")
+
     @staticmethod
     def yaw_to_quaternion(yaw):
         """Convert a yaw angle (in radians) to a quaternion."""
@@ -59,8 +101,8 @@ def parse_and_execute_command(robot: RobotInterface, command: str):
     """
     parts = command.split()
     
-    if len(parts) < 3:
-        print("Invalid command format. Use 'nav x y' or 'mov x y z'.")
+    if len(parts) < 2:
+        print("Invalid command format. Use 'nav x y' or 'mov x y z' or 'att obj_name' or 'det obj_name'.")
         return
 
     cmd_type = parts[0]
@@ -90,8 +132,20 @@ def parse_and_execute_command(robot: RobotInterface, command: str):
 
             robot.send_moveit_goal(target_pose)
 
+        elif cmd_type == "att" and len(parts) == 2:
+            obj_name = str(parts[1])
+            print(f"Attaching object: {obj_name}")
+
+            robot.send_attach_call(obj_name)
+
+        elif cmd_type == "det" and len(parts) == 2:
+            obj_name = str(parts[1])
+            print(f"Detaching object: {obj_name}")
+
+            robot.send_detach_call(obj_name)
+
         else:
-            print("Invalid command format. Use 'nav x y' or 'mov x y z'.")
+            print("Invalid command format. Use 'nav x y' or 'mov x y z' or 'att obj_name' or 'det obj_name'.")
     
     except ValueError:
         print("Error parsing command. Ensure x, y, z are valid numbers.")
@@ -123,7 +177,7 @@ def main(args=None):
     # robot.destroy_node()
     # rclpy.shutdown()
 
-    print("Enter commands in the format 'nav x y' or 'mov x y z'. Type 'exit' to quit.")
+    print("Enter commands in the format 'nav x y yaw' or 'mov x y z' or 'att obj_name' or 'det obj_name'. Type 'exit' to quit.")
     
     try:
         while True:
